ebd16f261b03857ba22b3b2faa3569a03ddcbc1f
bcgreen <bcgreen@bcgreen-linux>
2013-06-25 14:33:00 -0700

    updating media gallery module

--- profiles/openscholar/modules/contrib/media/js/media.popups.js
+++ profiles/openscholar/modules/contrib/media/js/media.popups.js
@@ -100,8 +100,6 @@ Drupal.media.popups.mediaBrowser = function (onSelect, globalOptions, pluginOpti
 
 Drupal.media.popups.mediaBrowser.mediaBrowserOnLoad = function (e) {
   var options = e.data;
-  if (this.contentWindow.Drupal.media.browser == undefined) return;
-
   if (this.contentWindow.Drupal.media.browser.selectedMedia.length > 0) {
     var ok = (Drupal && Drupal.t) ? Drupal.t('OK') : 'OK';
     var ok_func = $(this).dialog('option', 'buttons')[ok];
@@ -313,10 +311,7 @@ Drupal.media.popups.getDialogOptions = function () {
       backgroundColor: '#000000',
       opacity: 0.4
     },
-    zIndex: 10000,
-    close: function (event, ui) {
-      $(event.target).remove();
-    }
+    zIndex: 10000
   };
 };
 
